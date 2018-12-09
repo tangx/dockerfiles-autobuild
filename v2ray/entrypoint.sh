2771129a4f32a2703012f1e5b004c14b1e7851ea
tangxin <tangxin@tangxin-mbp.local>
2018-12-09 09:14:50 +0800

    update entrypoint.sh

--- v2ray/entrypoint.sh
+++ v2ray/entrypoint.sh
@@ -9,6 +9,8 @@ export ERROR_LOG=${ERROR_LOG:-"/dev/stdout"}
 export LOG_LEVEL=${LOG_LEVEL:-"warning"}
 export VMESS_UUID=${VMESS_UUID:-"12345678-0000-0000-0000-1234567890ab"}
 
-cat /etc/v2ray/${PROTOCOL}-config.json.tpl | envsubst > /etc/v2ray/config.json
+# cat /etc/v2ray/${PROTOCOL}-config.json.tpl | envsubst > /etc/v2ray/config.json
+
+envsubst < /etc/v2ray/${PROTOCOL}-config.json.tpl > /etc/v2ray/config.json 
 
 v2ray -config /etc/v2ray/config.json
